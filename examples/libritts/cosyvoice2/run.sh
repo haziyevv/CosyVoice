8b15458d177c6fb6682d0e68e5e2b33b94589765
EC2 Default User <ec2-user@ip-172-31-41-46.us-east-2.compute.internal>
2025-05-01 16:56:01 +0000

    updates for training

--- examples/libritts/cosyvoice2/run.sh
+++ examples/libritts/cosyvoice2/run.sh
@@ -2,31 +2,31 @@
 # Copyright 2024 Alibaba Inc. All Rights Reserved.
 . ./path.sh || exit 1;
 
-stage=-1
-stop_stage=3
+stage=6
+stop_stage=6
 
 data_url=www.openslr.org/resources/60
-data_dir=/mnt/lyuxiang.lx/data/tts/openslr/libritts
+data_dir=/mnt/nvme1/CosyVoice/train-data
 pretrained_model_dir=../../../pretrained_models/CosyVoice2-0.5B
 
-if [ ${stage} -le -1 ] && [ ${stop_stage} -ge -1 ]; then
-  echo "Data Download"
-  for part in dev-clean test-clean dev-other test-other train-clean-100 train-clean-360 train-other-500; do
-    local/download_and_untar.sh ${data_dir} ${data_url} ${part}
-  done
-fi
+# if [ ${stage} -le -1 ] && [ ${stop_stage} -ge -1 ]; then
+#   echo "Data Download"
+#   for part in dev-clean test-clean dev-other test-other train-clean-100 train-clean-360 train-other-500; do
+#     local/download_and_untar.sh ${data_dir} ${data_url} ${part}
+#   done
+# fi
 
 if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
   echo "Data preparation, prepare wav.scp/text/utt2spk/spk2utt"
-  for x in train-clean-100 train-clean-360 train-other-500 dev-clean dev-other test-clean test-other; do
+  for x in Twilight-Sparkle-data-train Twilight-Sparkle-data-test; do
     mkdir -p data/$x
-    python local/prepare_data.py --src_dir $data_dir/LibriTTS/$x --des_dir data/$x
+    python local/prepare_data.py --src_dir $data_dir/$x --des_dir data/$x
   done
 fi
 
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
   echo "Extract campplus speaker embedding, you will get spk2embedding.pt and utt2embedding.pt in data/$x dir"
-  for x in train-clean-100 train-clean-360 train-other-500 dev-clean dev-other test-clean test-other; do
+  for x in Twilight-Sparkle-data-train Twilight-Sparkle-data-test; do
     tools/extract_embedding.py --dir data/$x \
       --onnx_path $pretrained_model_dir/campplus.onnx
   done
@@ -34,7 +34,7 @@ fi
 
 if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
   echo "Extract discrete speech token, you will get utt2speech_token.pt in data/$x dir"
-  for x in train-clean-100 train-clean-360 train-other-500 dev-clean dev-other test-clean test-other; do
+  for x in Twilight-Sparkle-data-train Twilight-Sparkle-data-test; do
     tools/extract_speech_token.py --dir data/$x \
       --onnx_path $pretrained_model_dir/speech_tokenizer_v2.onnx
   done
@@ -42,7 +42,7 @@ fi
 
 if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
   echo "Prepare required parquet format data, you should have prepared wav.scp/text/utt2spk/spk2utt/utt2embedding.pt/spk2embedding.pt/utt2speech_token.pt"
-  for x in train-clean-100 train-clean-360 train-other-500 dev-clean dev-other test-clean test-other; do
+  for x in Twilight-Sparkle-data-train Twilight-Sparkle-data-test; do
     mkdir -p data/$x/parquet
     tools/make_parquet_list.py --num_utts_per_parquet 1000 \
       --num_processes 10 \
@@ -59,14 +59,14 @@ if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
     python cosyvoice/bin/inference.py --mode $mode \
       --gpu 0 \
       --config conf/cosyvoice2.yaml \
-      --prompt_data data/test-clean/parquet/data.list \
-      --prompt_utt2data data/test-clean/parquet/utt2data.list \
+      --prompt_data data/Twilight-Sparkle-data-test/parquet/data.list \
+      --prompt_utt2data data/Twilight-Sparkle-data-test/parquet/utt2data.list \
       --tts_text `pwd`/tts_text.json \
       --qwen_pretrain_path $pretrained_model_dir/CosyVoice-BlankEN \
       --llm_model $pretrained_model_dir/llm.pt \
       --flow_model $pretrained_model_dir/flow.pt \
       --hifigan_model $pretrained_model_dir/hift.pt \
-      --result_dir `pwd`/exp/cosyvoice/test-clean/$mode
+      --result_dir `pwd`/exp/cosyvoice/Twilight-Sparkle-data-test/$mode
   done
 fi
 
@@ -83,10 +83,10 @@ if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
   if [ $train_engine == 'deepspeed' ]; then
     echo "Notice deepspeed has its own optimizer config. Modify conf/ds_stage2.json if necessary"
   fi
-  cat data/{train-clean-100,train-clean-360,train-other-500}/parquet/data.list > data/train.data.list
-  cat data/{dev-clean,dev-other}/parquet/data.list > data/dev.data.list
+  cat data/Twilight-Sparkle-data-train/parquet/data.list > data/train.data.list
+  cat data/Twilight-Sparkle-data-test/parquet/data.list > data/dev.data.list
   # NOTE will update llm/hift training later
-  for model in llm flow; do
+  for model in llm flow hifigan; do
     torchrun --nnodes=1 --nproc_per_node=$num_gpus \
         --rdzv_id=$job_id --rdzv_backend="c10d" --rdzv_endpoint="localhost:1234" \
       cosyvoice/bin/train.py \
@@ -113,11 +113,11 @@ fi
 average_num=5
 if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
   for model in llm flow hifigan; do
-    decode_checkpoint=`pwd`/exp/cosyvoice/$model/$train_engine/${model}.pt
+    decode_checkpoint=`pwd`/exp/cosyvoice2/$model/$train_engine/${model}.pt
     echo "do model average and final checkpoint is $decode_checkpoint"
     python cosyvoice/bin/average_model.py \
       --dst_model $decode_checkpoint \
-      --src_path `pwd`/exp/cosyvoice/$model/$train_engine  \
+      --src_path `pwd`/exp/cosyvoice2/$model/$train_engine  \
       --num ${average_num} \
       --val_best
   done
