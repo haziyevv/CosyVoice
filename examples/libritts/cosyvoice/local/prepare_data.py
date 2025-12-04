b17da2ce9a03fcdd1d1fd9ba27be4b9e5c06d3c6
haziyev <farid@identityailabs.com>
2025-12-04 19:19:14 +0000

    training settings updated

--- examples/libritts/cosyvoice/local/prepare_data.py
+++ examples/libritts/cosyvoice/local/prepare_data.py
@@ -9,18 +9,31 @@ logger = logging.getLogger()
 
 
 def main():
-    wavs = list(glob.glob('{}/*/*/*wav'.format(args.src_dir)))
+    wavs = list(glob.glob('{}/*wav'.format(args.src_dir)))
 
     utt2wav, utt2text, utt2spk, spk2utt = {}, {}, {}, {}
     for wav in tqdm(wavs):
-        txt = wav.replace('.wav', '.normalized.txt')
+        txt = wav.replace('.wav', '.txt')
         if not os.path.exists(txt):
             logger.warning('{} do not exsist'.format(txt))
             continue
         with open(txt) as f:
             content = ''.join(l.replace('\n', '') for l in f.readline())
         utt = os.path.basename(wav).replace('.wav', '')
-        spk = utt.split('_')[0]
+        spk = args.src_dir.split('/')[-1]
+        
+        try:
+            prompt, content = content.split('<|endofprompt|>')
+        except:
+            print(content)
+            continue
+        if spk in {'Achernar', 'Aoede', 'Autonoe', 'Despina', 'Erinome', 'Kore', 'Leda', 'Pulcherrima', 'Sulafat', 'Vindemiatrix', 'Zephyr'}:
+            content = f"You are {spk}. Speak in a {prompt.strip()} tone<|endofprompt|>{content}"
+            #content = f"Speak in a {prompt.strip()} tone<|endofprompt|>{content}"
+        else:
+            content = f"You are Speaker_Actress. {prompt}<|endofprompt|>{content}"
+            #content = f"{prompt}<|endofprompt|>{content}"
+
         utt2wav[utt] = wav
         utt2text[utt] = content
         utt2spk[utt] = spk
--- examples/libritts/cosyvoice2/run.sh
+++ examples/libritts/cosyvoice2/run.sh
@@ -2,31 +2,36 @@
 # Copyright 2024 Alibaba Inc. All Rights Reserved.
 . ./path.sh || exit 1;
 
-stage=-1
-stop_stage=3
+stage=0
+stop_stage=5
 
 data_url=www.openslr.org/resources/60
-data_dir=/mnt/lyuxiang.lx/data/tts/openslr/libritts
-pretrained_model_dir=../../../pretrained_models/CosyVoice2-0.5B
+data_dir=/workspace/extracted_wavs
+pretrained_model_dir=../../../CosyVoice2-0.5B
 
-if [ ${stage} -le -1 ] && [ ${stop_stage} -ge -1 ]; then
-  echo "Data Download"
-  for part in dev-clean test-clean dev-other test-other train-clean-100 train-clean-360 train-other-500; do
-    local/download_and_untar.sh ${data_dir} ${data_url} ${part}
-  done
-fi
+train_folders=(
+  Autumn
+  Honey
+  Phoenix
+  Swiss
+)
+
+test_folder_name=(
+  test_data
+)
+
 
 if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
   echo "Data preparation, prepare wav.scp/text/utt2spk/spk2utt"
-  for x in train-clean-100 train-clean-360 train-other-500 dev-clean dev-other test-clean test-other; do
+  for x in "${train_folders[@]}" "${test_folder_name[@]}"; do
     mkdir -p data/$x
-    python local/prepare_data.py --src_dir $data_dir/LibriTTS/$x --des_dir data/$x
+    python local/prepare_data.py --src_dir $data_dir/$x --des_dir data/$x
   done
 fi
 
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
   echo "Extract campplus speaker embedding, you will get spk2embedding.pt and utt2embedding.pt in data/$x dir"
-  for x in train-clean-100 train-clean-360 train-other-500 dev-clean dev-other test-clean test-other; do
+  for x in "${train_folders[@]}" "${test_folder_name[@]}"; do
     tools/extract_embedding.py --dir data/$x \
       --onnx_path $pretrained_model_dir/campplus.onnx
   done
@@ -34,7 +39,7 @@ fi
 
 if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
   echo "Extract discrete speech token, you will get utt2speech_token.pt in data/$x dir"
-  for x in train-clean-100 train-clean-360 train-other-500 dev-clean dev-other test-clean test-other; do
+  for x in "${train_folders[@]}" "$test_folder_name"; do
     tools/extract_speech_token.py --dir data/$x \
       --onnx_path $pretrained_model_dir/speech_tokenizer_v2.onnx
   done
@@ -42,7 +47,7 @@ fi
 
 if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
   echo "Prepare required parquet format data, you should have prepared wav.scp/text/utt2spk/spk2utt/utt2embedding.pt/spk2embedding.pt/utt2speech_token.pt"
-  for x in train-clean-100 train-clean-360 train-other-500 dev-clean dev-other test-clean test-other; do
+  for x in "${train_folders[@]}" "$test_folder_name"; do
     mkdir -p data/$x/parquet
     tools/make_parquet_list.py --num_utts_per_parquet 1000 \
       --num_processes 10 \
@@ -52,22 +57,22 @@ if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
 fi
 
 # train llm
-export CUDA_VISIBLE_DEVICES="0,1,2,3"
+export CUDA_VISIBLE_DEVICES="0"
 num_gpus=$(echo $CUDA_VISIBLE_DEVICES | awk -F "," '{print NF}')
 job_id=1986
 dist_backend="nccl"
 num_workers=2
 prefetch=100
 train_engine=torch_ddp
-if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
+if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
   echo "Run train. We only support llm traning for now"
   if [ $train_engine == 'deepspeed' ]; then
     echo "Notice deepspeed has its own optimizer config. Modify conf/ds_stage2.json if necessary"
   fi
-  cat data/{train-clean-100,train-clean-360,train-other-500}/parquet/data.list > data/train.data.list
-  cat data/{dev-clean,dev-other}/parquet/data.list > data/dev.data.list
+  cat data/{Achernar,Aoede,Autonoe,Despina,Erinome,Kore,Leda,Pulcherrima,Sulafat,Vindemiatrix,Zephyr,Autumn,Honey,Phoenix,Swiss}/parquet/data.list > data/train.data.list
+  cat data/test_data/parquet/data.list > data/dev.data.list
   # NOTE will update llm/hift training later
-  for model in llm flow hifigan; do
+  for model in llm; do
     torchrun --nnodes=1 --nproc_per_node=$num_gpus \
         --rdzv_id=$job_id --rdzv_backend="c10d" --rdzv_endpoint="localhost:1234" \
       cosyvoice/bin/train.py \
@@ -92,13 +97,13 @@ fi
 
 # average model
 average_num=5
-if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
-  for model in llm flow hifigan; do
-    decode_checkpoint=`pwd`/exp/cosyvoice/$model/$train_engine/${model}.pt
+if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
+  for model in llm; do
+    decode_checkpoint=`pwd`/exp/cosyvoice2/$model/$train_engine/${model}.pt
     echo "do model average and final checkpoint is $decode_checkpoint"
     python cosyvoice/bin/average_model.py \
       --dst_model $decode_checkpoint \
-      --src_path `pwd`/exp/cosyvoice/$model/$train_engine  \
+      --src_path `pwd`/exp/cosyvoice2/$model/$train_engine  \
       --num ${average_num} \
       --val_best
   done
